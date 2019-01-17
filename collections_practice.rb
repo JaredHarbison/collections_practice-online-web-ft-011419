d4b0f33c1665ddf42dcb64d675439e396685de15
Jared Harbison <jared.harbison@gmail.com>
2019-01-17 00:27:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
+def sort_array_asc(array)
+  sorted = array.sort
+  sorted
+end
 
+def sort_array_desc(array)
+  sorted = array.sort! {|x, y| y <=> x}
+  sorted
+end
+
+def sort_array_char_count(array)
+  sorted = array.sort { |a,b| a.length <=> b.length }
+  sorted
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.collect do |element| 
+    element[2] = "$" 
+    element
+  end
+end 
+
+def find_a(array)
+  
+end
